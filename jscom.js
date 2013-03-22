85b80cefedbf0d7aa2e207b7a6b45f9847a88c14
Guy Bedford <guybedford@gmail.com>
2013-03-22 16:51:24 -0700

    define properly and make a global

--- jscom.js
+++ jscom.js
@@ -1,46 +1,50 @@
 define(['http-amd/json'], function(http) {
   var baseUrl = 'http://api.jspm.co';
-
-  login: function(username, password) {
-    http.post(baseUrl + '/login', {
-      username: username,
-      password: password
-    }, function(res) {
-      console.log(res);
-    }, function(err) {
-      console.log(err);
-    });
-  },
-  register: function(username, password, email) {
-    http.post(baseUrl + '/register', {
-      username: username,
-      password: password,
-      email: email
-    }, function(res) {
-      console.log(res);
-    }, function(err) {
-      console.log(err);
-    });
-  },
-  publish: function(name, endpoint, version) {
-    http.post(baseUrl + '/publish', {
-      name: name,
-      endpoint: endpoint,
-      version: version
-    }, function(res) {
-      console.log(res);
-    }, function(err) {
-      console.log(err);
-    });
-  },
-  publishAll: function(name, endpoint) {
-    http.post(baseUrl + '/publish_all', {
-      name: name,
-      endpoint
-    }, function(res) {
-      console.log(res);
-    }, function(err) {
-      console.log(err);
-    });
-  }
+  
+  var jscom = {
+    login: function(username, password) {
+      http.post(baseUrl + '/login', {
+        username: username,
+        password: password
+      }, function(res) {
+        console.log(res);
+      }, function(err) {
+        console.log(err);
+      });
+    },
+    register: function(username, password, email) {
+      http.post(baseUrl + '/register', {
+        username: username,
+        password: password,
+        email: email
+      }, function(res) {
+        console.log(res);
+      }, function(err) {
+        console.log(err);
+      });
+    },
+    publish: function(name, endpoint, version) {
+      http.post(baseUrl + '/publish', {
+        name: name,
+        endpoint: endpoint,
+        version: version
+      }, function(res) {
+        console.log(res);
+      }, function(err) {
+        console.log(err);
+      });
+    },
+    publishAll: function(name, endpoint) {
+      http.post(baseUrl + '/publish_all', {
+        name: name,
+        endpoint
+      }, function(res) {
+        console.log(res);
+      }, function(err) {
+        console.log(err);
+      });
+    } 
+  };
+  window.jscom = jscom;
+  return jscom;
 });
