47badcbdf4f3bb524f7eb03f46baa7a2442bbf17
guybedford <guybedford@gmail.com>
2014-03-11 11:13:16 -0700

    build and package fixes

---
+++ test/deps.js
@@ -0,0 +1,27 @@
+var config = require('../lib/config');
+
+suite('Configuration Dependency Parsing', function() {
+
+  test('NPM 1', function() {
+    var deps = config.parseDependencies({
+      three: '0.x.x'
+    }, 'npm');
+    assert.equal(deps['three'], 'npm:three@0');
+
+    deps = config.parseDependencies({
+      three: '0.0.x'
+    }, 'npm');
+    assert.equal(deps['three'], 'npm:three@0.0');
+
+    deps = config.parseDependencies({
+      inherits: '^2.0.1'
+    }, 'npm');
+    assert.equal(deps['inherits'], 'npm:inherits@^2.0.1');
+
+    deps = config.parseDependencies({
+      'base-64': '^0.0.4'
+    }, 'npm');
+    assert.equal(deps['base-64'], 'npm:base-64@^0.0.4');
+  });
+  
+});
