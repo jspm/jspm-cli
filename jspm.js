a75e539ddf4836e14bdb7b94c389bf2df0d8733f
guybedford <guybedford@gmail.com>
2014-09-27 17:54:06 +0200

    project structure, config wiring

--- jspm.js
+++ jspm.js
@@ -1,4 +1,21 @@
 #!/usr/bin/env node
+
+/*
+ *   Copyright 2014 Guy Bedford (http://guybedford.com)
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
 var Liftoff = require('liftoff');
 var path = require('path');
 
@@ -10,6 +27,7 @@ var jspmCLI = new Liftoff({
   }
 });
 jspmCLI.launch({}, function(env) {
+  process.env.jspmConfigPath = env.configPath;
   if (env.modulePath) {
     process.env.localJspm = true;
     require(path.resolve(env.modulePath, '../cli'));
