bc74b2edfff3f3f69e817f5f17542e0e09c2a064
guybedford <guybedford@gmail.com>
2015-01-29 18:13:02 +0200

    complete loader switching workflows

--- cli.js
+++ cli.js
@@ -81,7 +81,8 @@ process.on('uncaughtException', function(err) {
       + 'jspm link endpoint:name@version    Link a local folder as an installable package\n'
       + 'jspm install --link endpoint:name  Install a linked package\n'
       + '\n'
-      + 'jspm dl-loader [--edge --source]   Download the jspm browser loader\n'
+      + 'jspm dl-loader [--edge --source]   Download the browser loader files\n'
+      + 'jspm dl-loader [--6to5|--traceur]  Choose which ES6 transpiler to use\n'
       + '\n'
       + 'jspm setmode <mode>\n'
       + '  setmode local                    Switch to locally downloaded libraries\n'
@@ -270,10 +271,10 @@ process.on('uncaughtException', function(err) {
 
 
     case 'dl-loader':
-      var options = readOptions(args, ['--source', '--edge', '--yes']);
+      var options = readOptions(args, ['--source', '--edge', '--yes', '--6to5', '--traceur']);
       if (options.yes)
         ui.useDefaults();
-      core.dlLoader(options.source, options.edge);
+      core.dlLoader(options['6to5'] && '6to5' || options['traceur'] && 'traceur', options.source, options.edge);
     break;
 
     case 'setmode':
