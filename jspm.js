c9de6bd4863bf36557d0616fcf34149d28a96068
guybedford <guybedford@gmail.com>
2015-02-04 20:23:35 +0200

    update copyright

--- jspm.js
+++ jspm.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 /*
- *   Copyright 2014 Guy Bedford (http://guybedford.com)
+ *   Copyright 2014-2015 Guy Bedford (http://guybedford.com)
  *
  *   Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
