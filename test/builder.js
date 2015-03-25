b0d84d3d9fbdbb49454be0cdf2561d05f0a89e6d
guybedford <guybedford@gmail.com>
2015-03-25 02:13:32 +0200

    test adjustment

--- test/builder.js
+++ test/builder.js
@@ -6,7 +6,7 @@ api.setPackagePath('testlibs');
 suite('Build API', function() {
 
   test('Simple build API call', function(done) {
-    builder.build('[tests/*]')
+    builder.build('[unit/build/*]')
     .then(function(output) {
       assert(output.source.match(/\s*"format register";\s*/));
     })
