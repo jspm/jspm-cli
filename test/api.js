e6630526ffa5a6556f5a1cc47d66fa33c6e164ff
guybedford <guybedford@gmail.com>
2015-09-06 16:35:50 +0200

    api test install fix

--- test/api.js
+++ test/api.js
@@ -11,7 +11,11 @@ suite('API Calls', function() {
   });
 
   test('Normalize', function(done) {
-    api.normalize('jquery').then(function(normalized) {
+    return api.install('jquery', '2.1.4')
+    .then(function() {
+      return api.normalize('jquery');
+    })
+    .then(function(normalized) {
       assert(normalized === 'file://' + path.resolve('testlibs') + '/jspm_packages/github/components/jquery@2.1.4/jquery.js');
       done();
     })
