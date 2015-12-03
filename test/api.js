5291ceb5f4b1b252422f6d6851077b955c4e7ecb
guybedford <guybedford@gmail.com>
2015-12-03 18:05:11 +0200

    dogfooding, finally

--- test/api.js
+++ test/api.js
@@ -5,49 +5,39 @@ var path = require('path');
 
 api.setPackagePath('testlibs');
 
-suite('API Calls', function() {
+suite('API tests', function() {
+
   test('version', function() {
     assert(api.version.match(/\d+\.\d+\.\d+(-[^\.]+)?/));
   });
 
   test('Normalize', function(done) {
-    return api.install('jquery', '2.1.4')
-    .then(function() {
-      return api.normalize('jquery');
-    })
+    return api.normalize('jquery')
     .then(function(normalized) {
       assert(normalized === 'file://' + path.resolve('testlibs') + '/jspm_packages/github/components/jquery@2.1.4/jquery.js');
-      done();
     })
-    .catch(done);
+    .then(done, done);
+
   });
 
   test('Import', function(done) {
     api.dlLoader()
     .then(function() {
-      return api.install('text', '^0.0.2');
-    })
-    .then(function() {
       return api.import('text').then(function(text) {
         assert(text.translate);
-        done();
       });
     })
-    .catch(done);
+    .then(done, done);
+
   });
 
   test('Overrides', function(done) {
-    api.dlLoader()
-      .then(function() {
-        return api.install('ember', '1.13.2');
-      })
-      .then(function() {
-        return api.normalize('ember').then(function(normalized) {
-          assert(normalized === System.baseURL + 'jspm_packages/github/components/ember@1.13.2/ember.prod.js');
-          done();
-        });
-      })
-      .catch(done);
-  });
+    var loader = new api.Loader();
 
+    return api.normalize('ember')
+    .then(function(normalized) {
+      assert(normalized === loader.baseURL + 'jspm_packages/github/components/ember@1.13.2/ember.prod.js');
+    })
+    .then(done, done);
+  });
 });
