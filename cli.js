bd3140ae1b86524fc23ebaccc275c2d8e00f4506
guybedford <guybedford@gmail.com>
2016-01-19 20:56:08 +0200

    production and dev builds, link aliasing, command polishing

--- cli.js
+++ cli.js
@@ -445,7 +445,7 @@ process.on('uncaughtException', function(err) {
 
     case 'bundle':
       options = readOptions(args, ['inject', 'yes', 'skip-source-maps', 'minify',
-          'no-mangle', 'hires-source-maps', 'no-runtime', 'inline-source-maps', 'source-map-contents', 'browser', 'node', 'dev', 'skip-encode-names', 'skip-rollup'], 
+          'no-mangle', 'hires-source-maps', 'no-runtime', 'inline-source-maps', 'source-map-contents', 'browser', 'node', 'dev', 'production', 'skip-encode-names', 'skip-rollup'], 
           ['format', 'global-name', 'global-deps', 'global-defs', 'config', 'conditions']);
 
       if (options.yes)
