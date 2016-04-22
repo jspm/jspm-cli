92b30c0c35d69b04ca30c075996c23cd68dcbee0
guybedford <guybedford@gmail.com>
2016-04-22 03:21:01 +0200

    dotted property tests

--- test/cli-utils.js
+++ test/cli-utils.js
@@ -2,16 +2,21 @@ var readPropertySetters = require('../lib/cli-utils').readPropertySetters;
 
 suite('CLI Utils', function() {
   test('Simple property setters', function() {
-    var outObj = readPropertySetters('asdf.asdf=3 p=1 test=false with spa cing');
+    var outObj = readPropertySetters('asdf.asdf=3 p=1 test=false with spa cing', true);
     assert(outObj.asdf.asdf === 3);
     assert(outObj.p === 1);
     assert(outObj.test === 'false with spa cing');
   });
 
   test('Object syntax in setters', function() {
-    var outObj = readPropertySetters('a=spaced thing  b={an:"object",thing:false}');
+    var outObj = readPropertySetters('a=spaced thing  b={an:"object",thing:false}', true);
     assert(outObj.a === 'spaced thing');
     assert(outObj.b.an === 'object');
     assert(outObj.b.thing === false);
+  });
+
+  test('Dotted properties', function() {
+    var outObj = readPropertySetters('dotted.property=5', false);
+    assert(outObj['dotted.property'] === 5);
   });
 });
