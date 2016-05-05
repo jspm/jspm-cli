418673aab658022a2862c803836c51d90ba11eda
guybedford <guybedford@gmail.com>
2016-05-05 15:25:37 +0200

    boolean property setter test

--- test/cli-utils.js
+++ test/cli-utils.js
@@ -2,10 +2,11 @@ var readPropertySetters = require('../lib/cli-utils').readPropertySetters;
 
 suite('CLI Utils', function() {
   test('Simple property setters', function() {
-    var outObj = readPropertySetters('asdf.asdf=3 p=1 test=false with spa cing', true);
+    var outObj = readPropertySetters('asdf.asdf=3 p=1 test=false with spa cing actual=false', true);
     assert(outObj.asdf.asdf === 3);
     assert(outObj.p === 1);
     assert(outObj.test === 'false with spa cing');
+    assert(outObj.actual === false);
   });
 
   test('Object syntax in setters', function() {
