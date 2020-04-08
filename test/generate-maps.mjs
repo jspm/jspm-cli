32c72ee8fc5f4d182e521e297b0e0157c6ef6838
Guy Bedford <guybedford@gmail.com>
2020-04-07 21:42:00 -0700

    update generation

--- test/generate-maps.mjs
+++ test/generate-maps.mjs
@@ -7,19 +7,24 @@ const { TraceMap } = jspm;
 (async () => {
   const tests = eval(fs.readFileSync(fileURLToPath(import.meta.url + '/../test-list.json')).toString());
   const skip = eval(fs.readFileSync(fileURLToPath(import.meta.url + '/../skip-list.json')).toString());
+  let failures = 0;
+  let count = tests.length;
   for (const [index, test] of tests.entries()) {
-    if (skip.includes(test))
+    if (skip.includes(test)) {
+      count--;
       continue;
+    }
     const [installs] = typeof test === 'string' ? [test, test] : test;
     const path = fileURLToPath(import.meta.url + '/../maps/') + encodeURIComponent(installs) + '.json';
     if (fs.existsSync(path))
       continue;
-    console.log('Generating map for ' + test + ' (' + (index + 1) + ' / ' + tests.length + ')');
+    console.log('Generating map for ' + test + ' (' + (index + 1) + ' / ' + count + ' | ' + failures + ')');
     const map = new TraceMap(new URL('.', import.meta.url).href);
     try {
       await map.install(installs.split(' '));
     }
     catch (e) {
+      failures++;
       console.error(e);
       continue;
     }
