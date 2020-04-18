05615eca4e85779d7d4d9ab23d716423cf33ed61
Jarreddebeer <jarr2000@gmail.com>
2020-04-18 20:31:18 +0200

    adding spool&utils to skip-list for interop issue

--- test/skip-list.js
+++ test/skip-list.js
@@ -1,4 +1,7 @@
 [
   // TODO: Forgotten skip reason
-  "@adobe/spectrum-tokens"
+  "@adobe/spectrum-tokens",
+  // Named exports interop (https://github.com/jspm/jspm3/issues/43)
+  "@-0/spool",
+  "@-0/utils"
 ]
